1d7448114e6d6e5b3e832ccbcd17ec091da9147f
Vargoose84 <vargoose84@gmail.com>
2024-02-27 22:51:14 -0600

    add comments for suggested fix for shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -52,6 +52,7 @@ public class Shooter extends SubsystemBase implements Configable {
   private boolean idleEnable = true;
 
   private Shooter() {
+    //TODO: this motor should never be brake.
     m_shootMotor.getConfigurator().apply(new TalonFXConfiguration()
       .withMotorOutput(new MotorOutputConfigs()
         .withNeutralMode(NeutralModeValue.Brake)
